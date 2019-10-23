16c09e2b064ff3199b023c387d365795e7b1b20a
stevenzg <guoyinfeng@live.com>
2019-10-24 06:33:58 +0800

    feat: deploy static files to ali oss

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # ali-oss-action
+
+Deploy static pages to Ali OSS
+
+## Inputs
+
+### `region`
+
+**Required** oss region.
+
+### `accessKeyId`
+
+**Required** Your accessKeyId.
+
+### `accessKeySecret`
+
+**Required** Your accessKeySecret.
+
+### `bucket`
+
+**Required** Your bucket name.
+
+### `bucketPath`
+
+Directory under bucket.
+
+### `filePath`
+
+**Required** File directory to be uploaded. Default `"dist"`.
+
+## Outputs
+
+### `successCount`
+
+Success Count.
+
+### `failCount`
+
+Fail Count.
+
+## Example usage
+
+```yaml
+name: 'deploy-to-ali-oss'
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  ali_oss_deploy:
+    runs-on: ubuntu-latest
+    name: Deploy Pages to Ali OSS
+    steps:
+      - name: Checkout
+        uses: actions/checkout@master
+      - run: yarn install
+      - run: yarn build
+      - name: Deploy
+        id: DeployOss
+        uses: stevenzg/ali-oss-action@master
+        with:
+          region: ${{ secrets.region }}
+          accessKeyId: ${{ secrets.accessKeyId }}
+          accessKeySecret: ${{ secrets.accessKeySecret }}
+          bucket: ${{ secrets.bucket }}
+          filePath: 'dist'
+      - name: Deploy Result
+        run: echo "${{ steps.DeployOss.outputs.successCount }} success, ${{ steps.DeployOss.outputs.failCount }} fail."
+```
---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ * Copied some code from:
+ * https://github.com/hustcc/aliyun-oss-deploy
+ */
+
+const core = require('@actions/core')
+const path = require('path')
+const fs = require('fs')
+const globby = require('globby')
+const OSS = require('ali-oss')
+
+const fileList = filePath =>
+  fs.statSync(filePath).isFile()
+    ? [filePath]
+    : globby.sync(['**/*.*'], { cwd: filePath })
+
+void (async function() {
+  try {
+    const region = core.getInput('region')
+    const accessKeyId = core.getInput('accessKeyId')
+    const accessKeySecret = core.getInput('accessKeySecret')
+    const bucket = core.getInput('bucket')
+    const bucketPath = core.getInput('bucketPath')
+    const filePath = core.getInput('filePath')
+
+    const client = new OSS({
+      region,
+      accessKeyId,
+      accessKeySecret,
+      bucket
+    })
+
+    const pageFilesPath = path.resolve(filePath)
+    const files = fileList(pageFilesPath)
+
+    let successCount = 0
+    let failCount = 0
+    for (const file of files) {
+      const ossTarget = path.join(bucketPath, file)
+      const stream = fs.createReadStream(path.join(pageFilesPath, file))
+      const result = await client.putStream(ossTarget, stream, {
+        timeout: 720000
+      })
+      console.log('Result:', result)
+      if (result.res.status === 200) {
+        ++successCount
+      } else {
+        ++failCount
+      }
+    }
+
+    core.setOutput('successCount', successCount)
+    core.setOutput('failCount', failCount)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
